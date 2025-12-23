3f741ecb1f882a3ac447570634e8573caa8cfaad
0bscene-Heathen <132075815+0bscene-Heathen@users.noreply.github.com>
2025-12-23 22:11:25 +0200

    Finish coding the game
    Create the game logic, playRound function logic, choics, score, and
    playGame logic that finishes the game in 5 rounds and declares the
    winner after comparing score.

--- script.js
+++ script.js
@@ -12,3 +12,62 @@ function getComputerChoice() {
     }
 }
 
+// Get the human choice and return it
+function getHumanChoice() {
+    return prompt("Type Rock, Paper, or Scissors.");
+}
+
+// Declare User score and Computer Score 
+let humanScore = 0;
+let computerScore = 0;
+
+// Game round logic 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
+    computerChoice = computerChoice[0].toUpperCase() + computerChoice.slice(1).toLowerCase();
+
+    if (humanChoice === "Rock" && computerChoice === "Paper") {
+        computerScore += 1;
+        console.log(`You lose!, ${computerChoice} beats ${humanChoice}`)
+    }
+    else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        humanScore += 1;
+        console.log(`You Win!, ${humanChoice} beats ${computerChoice}`)
+    }
+    else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        humanScore += 1;
+        console.log(`You Win!, ${humanChoice} beats ${computerChoice}`)
+    }
+    else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        computerScore += 1;
+        console.log(`You lose!, ${computerChoice} beats ${humanChoice}`)
+    }
+    else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        humanScore += 1;
+        console.log(`You Win!, ${humanChoice} beats ${computerChoice}`)
+    }
+    else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        computerScore += 1;
+        console.log(`You lose!, ${computerChoice} beats ${humanChoice}`)
+    }
+    else {
+        console.log("Tie!")
+    }
+}
+
+// Game logic
+function playGame() {
+    for(let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You Won The Game!");
+    } else {
+        console.log("You Lost! :(");
+    }
+}
+
+playGame()
